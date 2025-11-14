0bddb43c3a63a28bb7bb99233daf7c8063fef56a
Devin <devinlainediaz@gmail.com>
2025-11-13 22:47:07 -0500

    integrate angular with backend

---
+++ frontend/src/app/api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'http://localhost:8080';
+  constructor(private http: HttpClient) { }
+
+  getMessage(){
+    return this.http.get(`${this.apiUrl}/`)
+  }
+}
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './api.service'
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  message: string;
+  constructor(private apiService: ApiService){}
+  ngOnInit() {
+    this.apiService.getMessage().subscribe(
+      (data:any) => {this.message = data;},
+      (error: any) => {console.error('An error occurred:', error);}
+    )
+  }
   title = 'frontend';
 }
