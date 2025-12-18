5cd1584fed776efd42341c6ca58c16ee08a5cb5f
Devin <devinlainediaz@gmail.com>
2025-12-18 14:55:16 -0500

    add multi-select chips for genres

--- frontend/src/app/create-modal/create-modal.component.ts
+++ frontend/src/app/create-modal/create-modal.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create-modal',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './create-modal.component.html',
   styleUrl: './create-modal.component.css'
 })
@@ -20,6 +21,43 @@ export class CreateModalComponent {
     photo_file_path: '',
     notes: ''
   }
+
+  genresList = [
+    'Alternative',
+    'Bedroom pop',
+    'Chillwave',
+    'Dream pop',
+    'Electronic',
+    'Experimental',
+    'Garage rock',
+    'Indie',
+    'Indie pop',
+    'Indie rock',
+    'Lo-fi',
+    'Neo-psychedelic',
+    'New wave',
+    'Noise rock',
+    'Pop',
+    'Post-punk',
+    'Post-rock',
+    'Psychedelic rock',
+    'Shoegaze',
+    'Sovietwave',
+    'Surf rock',
+    'Synth-pop',
+    'Synthwave',
+    'Vaporwave',
+    'Witch house'
+  ];
+
+  toggleGenre(genre: string) {
+  const i = this.vinyl.genres.indexOf(genre);
+  if (i > -1) {
+    this.vinyl.genres.splice(i, 1);
+  } else {
+    this.vinyl.genres.push(genre);
+  }
+}
 
   createVinyl() {
     this.create.emit(this.vinyl);
