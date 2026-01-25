fe734dcfbf58580e468fcdde92469ae0cde6c44f
Devin <devinlainediaz@gmail.com>
2026-01-25 16:19:42 -0500

    fix carousel logic & html

--- frontend/src/app/carousel/carousel.component.ts
+++ frontend/src/app/carousel/carousel.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-carousel',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css'
 })
