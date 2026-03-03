2efbc7aacf08a4c8ae3604e4e3da5201cd373841
Devin <devinlainediaz@gmail.com>
2026-03-02 19:34:50 -0500

    add existing vinyl images to landing page carousel

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CarouselComponent } from '../carousel/carousel.component';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [CarouselComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
-  constructor(private router: Router) {}
+  vinyls: any[] = []
+  vinylImages: string[] = [];
+  
+  constructor(private router: Router,
+              private apiService: ApiService
+  ) {}
+
+  ngOnInit(){
+    this.apiService.getVinyls().subscribe((data: any) => {
+      this.vinyls = data;
+      this.setVinylImages();
+    });
+  }
+
+  setVinylImages() {
+    this.vinylImages = this.vinyls.map(vinyl => vinyl.photo_file_path);
+  }
 
   goToCollection() {
     this.router.navigate(['/collection']);
